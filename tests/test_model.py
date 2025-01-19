c81764d9ea118c12f9508dacc6d833acd2e2a1f6
soender2 <s214925@dtu.dk>
2025-01-19 15:06:13 +0100

    finishing pytest and ensured that coverage run worked

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,14 +1,12 @@
-from src.healthy_vs_rotten.model import FruitClassifier, ModelParams
+from src.healthy_vs_rotten.model import FruitClassifier
 import pytest
 from omegaconf import OmegaConf
 import torch
 
-@pytest.mark.parametrize("batch_size", [32])
+@pytest.mark.parametrize("batch_size", [32, 64])
 def test_model(batch_size : int):
-    cfg = OmegaConf.load("configs/model_experiments/model1.yaml")
-    cfg = OmegaConf.to_container(cfg, resolve=True)["model_params"]
-    params = ModelParams(**cfg)
-    model = FruitClassifier(params)
+    cfg = OmegaConf.load("configs/model/default.yaml")
+    model = FruitClassifier(cfg["pretrained_model_name"], cfg["classifier"])
     x = torch.randn(batch_size, 3, 28, 28)
     y = model(x)
     assert y.shape == (batch_size, 1)
